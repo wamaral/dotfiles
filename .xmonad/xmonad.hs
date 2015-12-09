526b68886c1db237eb9e0f1f0966b3f2a8f64022
Wagner Amaral <wamaral@bionexo.com>
2015-12-09 15:39:02 -0200

    Add kill xfdesktop key

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -159,6 +159,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_Tab), shiftNextScreen >> nextScreen) -- window to next xinerama screen
     , ((modm .|. controlMask, xK_Tab), swapNextScreen >> nextScreen) -- swap xinerama screens
 
+    -- Kill xfdesktop (automatically starts after plugging external monitor)
+    , ((modm, xK_q), spawn "killall xfdesktop")
+
     -- Quit xmonad
     , ((modm .|. controlMask, xK_q), io exitSuccess)
 
