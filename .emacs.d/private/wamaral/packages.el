e7345d063fe5aa185db6dfa65683e8e2f257a82f
Wagner Amaral <wamaral@wamaral.org>
2018-07-10 18:40:22 -0300

    Spacemacs custom config cleanup

--- .emacs.d/private/wamaral/packages.el
+++ .emacs.d/private/wamaral/packages.el
@@ -34,13 +34,10 @@
     flycheck-clojure
     helm-cider
     helm-flycheck
-    dired+
     tern-auto-complete
     evil-smartparens
     evil-embrace
     projectile-direnv
-    highlight-chars
-    magithub
     (evil-little-word :location (recipe
                                  :fetcher github
                                  :repo "tarao/evil-plugins"
@@ -92,11 +89,6 @@ Each entry is either:
   (require 'helm-flycheck)
   (spacemacs/set-leader-keys "ee" 'helm-flycheck))
 
-(defun wamaral/init-dired+ ()
-  (require 'dired-x)
-  (require 'dired+)
-  (setq dired-recursive-deletes 'top))
-
 (defun wamaral/init-tern-auto-complete ()
   (eval-after-load 'tern
     '(progn
@@ -118,36 +110,26 @@ Each entry is either:
 
 (defun wamaral/init-evil-little-word ()
   (use-package evil-little-word
-               :commands (evil-forward-little-word-begin
-                           evil-backward-little-word-begin
-                           evil-forward-little-word-end
-                           evil-backward-little-word-end
-                           evil-a-little-word
-                           evil-inner-little-word)
-               :init (progn
-                       (define-key evil-motion-state-map (kbd "glw") 'evil-forward-little-word-begin)
-                       (define-key evil-motion-state-map (kbd "glb") 'evil-backward-little-word-begin)
-                       (define-key evil-motion-state-map (kbd "glW") 'evil-forward-little-word-end)
-                       (define-key evil-motion-state-map (kbd "glB") 'evil-backward-little-word-end)
-                       (define-key evil-outer-text-objects-map (kbd "lw") 'evil-a-little-word)
-                       (define-key evil-inner-text-objects-map (kbd "lw") 'evil-inner-little-word))))
-
-(defun wamaral/init-magithub ()
-  (use-package magithub
-               :after magit
-               :ensure t
-               :config (magithub-feature-autoinject t)))
+    :commands (evil-forward-little-word-begin
+               evil-backward-little-word-begin
+               evil-forward-little-word-end
+               evil-backward-little-word-end
+               evil-a-little-word
+               evil-inner-little-word)
+    :init (progn
+            (define-key evil-motion-state-map (kbd "glw") 'evil-forward-little-word-begin)
+            (define-key evil-motion-state-map (kbd "glb") 'evil-backward-little-word-begin)
+            (define-key evil-motion-state-map (kbd "glW") 'evil-forward-little-word-end)
+            (define-key evil-motion-state-map (kbd "glB") 'evil-backward-little-word-end)
+            (define-key evil-outer-text-objects-map (kbd "lw") 'evil-a-little-word)
+            (define-key evil-inner-text-objects-map (kbd "lw") 'evil-inner-little-word))))
 
 (defun wamaral/init-evil-ruby-block-object ()
   (add-hook 'ruby-mode
             (lambda () (require 'evil-ruby-block-object))))
 
-(defun wamaral/highlight-chars ()
-  (require 'highlight-chars)
-  (add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
-  (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace))
-
 (defun wamaral/init-wamaral ()
-  (add-to-list 'auto-mode-alist '("\\Jenkinsfile\\'" . groovy-mode)))
+  (add-to-list 'auto-mode-alist '("Jenkinsfile\\'" . groovy-mode))
+  (add-to-list 'auto-mode-alist '("\\.aurora\\'" . python-mode)))
 
 ;;; packages.el ends here
